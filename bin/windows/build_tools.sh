379aa82e03f3e7000e1f32e7cbc138e45ee61ad0
luo hao <990747088@qq.com>
2019-08-03 20:07:58 +0800

    add windows build tools

---
+++ bin/windows/build_tools.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export GOROOT=/usr/local/go
+export GOOS=windows
+
+# api tool
+go build -o api ../../api/main.go
+
+# code tool
+go build -o code ../../code/main.go
+
+## log formatter
+#go build -o logfmt ${GOPATH}/src/github.com/haozzzzzzzz/go-rapid-development/tools/logfmt/main.go
+#
+## lambda build
+#go build -o lamb ${GOPATH}/src/github.com/haozzzzzzzz/go-lambda/tools/lamb/main.go
+#
+## lambda deploy
+#go build -o lamd ${GOPATH}/src/github.com/haozzzzzzzz/go-lambda/tools/lamd/main.go
+
+echo finish
