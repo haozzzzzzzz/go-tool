5b80954063633b1ade6fedec963442c6e7bd6673
luo hao <990747088@qq.com>
2019-09-11 00:09:44 +0800

    modify

--- api/examples/test_doc_gin_func/api/api.go
+++ api/examples/test_doc_gin_func/api/api.go
@@ -1,16 +1,49 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type HeaderData struct {
+	H1 string `json:"h_1"`
+}
+
+type UriData struct {
+	U1 string `json:"u1"`
+}
+
+type QueryData struct {
+	Q1 string `json:"q_1"` // haha
+}
+
+type PostData struct {
+	P1 string `json:"p_1"`
+}
+
 /*
-sfaasdfasd
+测试接口名字
+描述1
 @api_doc_http_method: GET
-@api_doc_relative_paths: /test/say_hi
+描述1
+
+@api_doc_relative_paths: /test/say_hi/:u1
+描述1
+
 */
 func SayHiGinHandlerFunc(context *gin.Context) {
+
+	headerData := &HeaderData{}
+	uriData := &UriData{}
+	queryData := &QueryData{}
+	postData := &PostData{}
+
+	_ = queryData
+	_ = uriData
+	_ = headerData
+	_ = postData
+
 	// response data
 	type ResponseData struct {
 		Msg string `json:"msg"` // 问候信息
